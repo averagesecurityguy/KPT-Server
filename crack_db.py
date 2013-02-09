73b99691c48f5ecb9b653f912334cdfa0a0f9b78
Stephen Haywood <stephen@averagesecurityguy.info>
2013-02-08 21:18:28 -0500

    Multiple updates
    Fixed crack_password logic to only check LM hashes if NTLM failed.
    Only add uncracked passwords to the DB if both lookups failed.
    Added checks to ensure uploaded hashes are not malicious.

--- crack_db.py
+++ crack_db.py
@@ -2,6 +2,9 @@ import redis
 import hashlib
 import binascii
 import itertools
+import re
+
+hash_re = re.compile(r'[0-9A-Fa-f]{32}')
 
 user_db = redis.StrictRedis(host='localhost', port=6379, db=0)
 lm_db = redis.StrictRedis(host='localhost', port=6379, db=1)
@@ -9,13 +12,14 @@ nt_db = redis.StrictRedis(host='localhost', port=6379, db=2)
 ucnt_db = redis.StrictRedis(host='localhost', port=6379, db=3)
 uclm_db = redis.StrictRedis(host='localhost', port=6379, db=4)
 
+
 def convert_lm_to_ntlm(lm_plain, nt):
     combos = map(''.join, itertools.product(*((c.upper(),
                                                c.lower()) for c in lm_plain)))
 
     for c in combos:
         hash = hashlib.new('md4', c.encode('utf-16le')).digest()
-        if binascii.hexlify(hash) == nt:
+        if binascii.hexlify(hash).upper() == nt:
             return c
 
     return None
@@ -28,23 +32,36 @@ def crack_passwords(request):
 
     cracked = []
     for p in request:
+        # Verify the data we were sent looks like a hash. If not, set them to
+        # empty strings.
+        m = hash_re(p['lm'].upper())
+        if m is None:
+            p['lm'] = ''
+
+        m = hash_re(p['nt'].upper())
+        if m is None:
+            p['nt'] = ''
+
+        # Process the hashes to find the plaintext. If the NTLM has is in the
+        # database, then use it. If it is not, then see if the LM hash is in
+        # the database and convert it to NTML to ensure we have the correct
+        # case.
         lm_plain = lm_db.get(p['lm'].upper())
         nt_plain = nt_db.get(p['nt'].upper())
 
         if nt_plain is not None:
             update_nt_count(p['nt'].upper(), 1)
-            cracked.append({p['nt']: nt_plain})
+            cracked.append({p['nt'].upper(): nt_plain})
         else:
-            ucnt_db.set(p['nt'], '')
-
-        if lm_plain is not None:
-            update_lm_count(p['lm'].upper(), 1)
-            nt_plain = convert_lm_to_ntlm(lm_plain, p['nt'])
-            if nt_plain is not None:
-                nt_db.set(p['nt'], nt_plain)
-                cracked.append({p['nt']: nt_plain})
-        else:
-            uclm_db.set(p['lm'], '')
+            if lm_plain is not None:
+                update_lm_count(p['lm'].upper(), 1)
+                nt_plain = convert_lm_to_ntlm(lm_plain, p['nt'].upper())
+                if nt_plain is not None:
+                    nt_db.set(p['nt'].upper(), nt_plain)
+                    cracked.append({p['nt'].upper(): nt_plain})
+            else:
+                ucnt_db.set(p['nt'].upper(), '')
+                uclm_db.set(p['lm'].upper(), '')
 
     return cracked
 
