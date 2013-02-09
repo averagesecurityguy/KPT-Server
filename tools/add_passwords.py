3a26255bfbf9ff4b26cc32c76740950b70b06fbc
Stephen Haywood <stephen@averagesecurityguy.info>
2013-02-08 22:48:29 -0500

    Read file from command line. Updated to match schema change.

--- tools/add_passwords.py
+++ tools/add_passwords.py
@@ -1,5 +1,11 @@
 import redis
 import smbpasswd
+import sys
+import json
+
+if len(sys.argv) != 2:
+    print 'USAGE: add_passwords.py file'
+    sys.exit()
 
 lm_db = redis.StrictRedis(host='localhost', port=6379, db=1)
 nt_db = redis.StrictRedis(host='localhost', port=6379, db=2)
@@ -7,22 +13,18 @@ nt_db = redis.StrictRedis(host='localhost', port=6379, db=2)
 
 def add_lm_hash(hash, plain):
     print 'Adding LM hash {0}'.format(hash)
-    lm_db.set(hash, plain)
-    print lm_db.get(hash)
-    lm_db.set(hash + ':count', 0)
-    print lm_db.get(hash + ':count')
+    lm = {'plain': plain, 'count': 0}
+    lm_db.set(hash.upper(), json.dumps(lm))
 
 
 def add_nt_hash(hash, plain):
     print 'Adding NT hash {0}'.format(hash)
-    nt_db.set(hash, plain)
-    print nt_db.get(hash)
-    nt_db.set(hash + ':count', 0)
-    print nt_db.get(hash + ':count')
+    nt = {'plain': plain, 'count': 0}
+    nt_db.set(hash.upper(), json.dumps(nt))
 
-for line in open('passwords.txt'):
+for line in open(sys.argv[1]
+):
     line = line.rstrip('\r\n')
     print line
     add_lm_hash(smbpasswd.lmhash(line), line)
     add_nt_hash(smbpasswd.nthash(line), line)
-
