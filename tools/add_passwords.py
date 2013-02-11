7d4c0ec377322667b5d414dbe8577575f2693c8a
Stephen Haywood <stephen@averagesecurityguy.info>
2013-02-11 03:16:59 +0000

    Removed print statements.

--- tools/add_passwords.py
+++ tools/add_passwords.py
@@ -12,20 +12,20 @@ nt_db = redis.StrictRedis(host='localhost', port=6379, db=2)
 
 
 def add_lm_hash(hash, plain):
-    print 'Adding LM hash {0}'.format(hash)
+    # print 'Adding LM hash {0}'.format(hash)
     lm = {'plain': plain, 'count': 0}
     lm_db.set(hash.upper(), json.dumps(lm))
 
 
 def add_nt_hash(hash, plain):
-    print 'Adding NT hash {0}'.format(hash)
+    # print 'Adding NT hash {0}'.format(hash)
     nt = {'plain': plain, 'count': 0}
     nt_db.set(hash.upper(), json.dumps(nt))
 
 for line in open(sys.argv[1]
 ):
     line = line.rstrip('\r\n')
-    print line
+    # print line
 
     # LM truncates at 14 characters.
     if len(line) <= 14:
