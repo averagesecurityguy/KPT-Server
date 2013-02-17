c35a7c494d7e54edb64102c7ae36263719155463
Stephen Haywood <stephen@averagesecurityguy.info>
2013-02-17 01:29:30 +0000

    Write uncracked hashes to a pwdump file for cracking.

--- tools/get_uncracked.py
+++ tools/get_uncracked.py
@@ -6,6 +6,7 @@ if len(sys.argv) != 1:
     sys.exit()
 
 uc_db = redis.StrictRedis(host='localhost', port=6379, db=3)
+outfile = open('uncracked.pwdump', 'w')
 
 count = 0
 for k in uc_db.keys():
@@ -13,5 +14,10 @@ for k in uc_db.keys():
     uid = count
     lm = uc_db.get(k)
     nt = k
-    print '{0}:{1}:{2}:{3}:::'.format(user, uid, lm, nt)
+    outfile.write('{0}:{1}:{2}:{3}:::'.format(user, uid, lm, nt))
     count += 1
+
+outfile.flush()
+outfile.close()
+
+uc_db.flushdb()
