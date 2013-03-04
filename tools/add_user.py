b6a488ff74f1445a71d0f6a26fcf09268ceadb76
Stephen Haywood <stephen@averagesecurityguy.info>
2013-03-03 20:49:20 -0500

    Multiple Fixes
    Added check to ensure duplicate accounts are not created.
    Added a days question so I can set the license to expire in 7, 30,
    or 365 days.

--- tools/add_user.py
+++ tools/add_user.py
@@ -4,6 +4,7 @@ import time
 import random
 import string
 import json
+import sys
 
 user_db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -15,8 +16,15 @@ def generate_random_string(l):
     return "".join([random.choice(key_space) for x in xrange(l)])
 
 
-def add_new_user(email, hash_max):
+def add_new_user(email, hash_max, days):
     print 'Creating new user account.'
+
+    # Don't overwrite an existing user.
+    resp = user_db.get(email)
+    if resp is not None:
+        print 'Error: Email address is already in the database.'
+        sys.exit(1)
+
     ck = generate_random_string(16)
     cs = generate_random_string(32)
     at = generate_random_string(16)
@@ -28,7 +36,7 @@ def add_new_user(email, hash_max):
     user_db.set(ck + ':consumer_secret', cs)
     user_db.set(ck + ':access_token', at)
     user_db.set(ck + ':access_token_secret', ats)
-    user_db.set(ck + ':expiration', int(time.time()) + 365 * 24 * 60 * 60)
+    user_db.set(ck + ':expiration', int(time.time()) + (days * 24 * 60 * 60))
     user_db.set(ck + ':cracked_count', 0)
     user_db.set(ck + ':hash_max', hash_max) 
     user_db.set(ck + ':hash_count', 0)
@@ -53,5 +61,6 @@ def generate_license_key(ck, cs, at, ats):
 print 'Add a new user account to the database.'
 email = raw_input('Enter the email address of the user: ')
 key_file_name = email.replace('@', '_at_').replace('.', '_dot_') + '.key'
-hash_max = raw_input('Enter the maximum number of submitted hashes: ') 
-add_new_user(email, int(hash_max))
+hash_max = raw_input('Enter the maximum number of submitted hashes: ')
+days = raw_input('Enter the number of days the license should last: ') 
+add_new_user(email, int(hash_max), days)
